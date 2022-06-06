d353f5291d040884c7d664d97b7a234ccb45ec4e
Gustavo Costa <gucostapereira777@gmail.com>
2022-06-06 11:54:54 -0300

    hbcja

--- Lista5/Atividade6.java
+++ Lista5/Atividade6.java
@@ -9,7 +9,7 @@ public class Atividade6 {
         trocaColuna4PorColuna10();
         trocaDiagonalPrincipalComDiagonalSecundaria();
         trocaLinha5ComColuna10();
-        System.out.println("ola julia");
+        System.out.println("ola julia 2"); 
     }
 
     private static void preencherMatrizPrincipal() {
