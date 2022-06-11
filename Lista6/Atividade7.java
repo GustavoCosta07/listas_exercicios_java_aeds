52d88d944ce1a9cb143bfb9fbabc347ab46dee8d
Gustavo Costa <gucostapereira777@gmail.com>
2022-06-11 16:20:15 -0300

    Atividade 7 lista 6

--- Lista6/Atividade7.java
+++ Lista6/Atividade7.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
 
-public class Atividade7  {
+public class Atividade7 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner (System.in);
-        
-
+        System.out.println("Digite uma frase: ");
+        String frase = entrada.nextLine().toUpperCase().trim();
+        System.out.println(frase);
         entrada.close();
     }
 }
