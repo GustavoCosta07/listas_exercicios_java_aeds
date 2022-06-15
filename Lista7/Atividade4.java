28cc487bb3b27f819c6fb6fbe74c0bcf0ce3629a
Gustavo Costa <gucostapereira777@gmail.com>
2022-06-15 15:12:17 -0300

    atividade 4 lista 7

---
+++ Lista7/Atividade4.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+public class Atividade4 {
+    public static void main(String[] args) throws IOException {
+        Scanner entrada = new Scanner (System.in);
+        
+      
+        Path caminho = Paths.get("arquivo_questao3.txt");
+        try {
+            byte[] texto = Files.readAllBytes(caminho);
+            String leitura = new String(texto);
+            String[] textoSeparado = leitura.split("\n");
+            for (int i = 0; i < textoSeparado.length; i++) {
+                System.out.println(textoSeparado[i]);
+            }
+
+            int controle = 0; 
+            for (int i = 0; i < textoSeparado.length; i++) {
+                controle ++;
+            }
+            System.out.println("Este arquivo possui "+controle+" Linhas");
+            // aconteceu algo estranho, quando coloquei o for pra iterar com tamanho de texto.lentgh, ele ignorava o sysout
+            
+            
+        } catch (Exception erro) {
+        }
+        
+        entrada.close();
+    }
+}
