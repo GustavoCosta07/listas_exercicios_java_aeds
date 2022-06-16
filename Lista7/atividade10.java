3093321e3bf0b99a019b21b2a35f528167e9b37b
Gustavo Costa <gucostapereira777@gmail.com>
2022-06-16 17:22:13 -0300

    Atividade 10 lista 7

--- Lista7/atividade10.java
+++ Lista7/atividade10.java
@@ -2,17 +2,30 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Atividade10 {
     public static void main(String[] args) throws IOException {
         Scanner entrada = new Scanner (System.in);
+        int [] vetor = new int [20];
+        int controle =0;
         System.out.println("Digite 20 números para serem gravados no arquivo: ");
-        for (int i = 0; i <= 20; i++) {
+        for (int i = 0; i < vetor.length; i++) {   
             int numeros = entrada.nextInt();
+            vetor [i] = numeros; 
+            controle += numeros;
             criarArquivo("arquivo.txt", numeros);
         }
-
+        Arrays.sort(vetor); //isso ordena do menor para o maior
+        int media = controle /20;
+        
+        
+        int maior=vetor[19]; //o maior valor fica na última posição
+        int menor=vetor[0]; // e o menor na primeira
+        
+        System.out.println("Maior = "+maior+"\nMenor = "+menor+"\nMedia ="+media);
+  
         entrada.close();
     }
     
